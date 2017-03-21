f692607175370bd11a7252d2b508e4e49e82637d
Heidi Rodriguez <hrod2645@gmail.com>
2017-03-20 18:41:18 -0600

    add authentication and spinner for user

--- auth/auth/src/app.js
+++ auth/auth/src/app.js
@@ -1,14 +1,26 @@
 'use strict';
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA_eXpUzy-hNMGom-o_qDGTxgNIO9LW8QA',
+      authDomain: 'auth-3cd69.firebaseapp.com',
+      databaseURL: 'https://auth-3cd69.firebaseio.com',
+      storageBucket: 'auth-3cd69.appspot.com',
+      messagingSenderId: '449720555812'
+    });
+  }
+
   render() {
     return(
       <View>
         <Header headerText="Authentication" />
-        <Text>Auth</Text>
+        <LoginForm />
       </View>
     );
   }
