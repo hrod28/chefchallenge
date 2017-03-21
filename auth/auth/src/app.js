209ce24ce0773eead2735d5e393b2d7f052312d6
Heidi Rodriguez <hrod2645@gmail.com>
2017-03-21 12:58:08 -0600

    add chef_stack

--- auth/auth/src/app.js
+++ auth/auth/src/app.js
@@ -1,43 +1,53 @@
+'use strict';
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Spinner } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
   state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
-      authDomain: 'authentication-70a18.firebaseapp.com',
-      databaseURL: 'https://authentication-70a18.firebaseio.com',
-      storageBucket: 'authentication-70a18.appspot.com',
-      messagingSenderId: '682333809338'
+      apiKey: 'AIzaSyA_eXpUzy-hNMGom-o_qDGTxgNIO9LW8QA',
+      authDomain: 'auth-3cd69.firebaseapp.com',
+      databaseURL: 'https://auth-3cd69.firebaseio.com',
+      storageBucket: 'auth-3cd69.appspot.com',
+      messagingSenderId: '449720555812'
     });
 
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged((user)=>{
       if (user) {
         this.setState({ loggedIn: true });
-      } else {
+      }
+      else {
         this.setState({ loggedIn: false });
       }
-    });
+    })
   }
 
   renderContent() {
-    switch (this.state.loggedIn) {
-      case true:
-        return <Button>Log In</Button>;
-      case false:
-        return <LoginForm />;
-      default:
-        return <Spinner size="large" />;
+      switch (this.state.loggedIn) {
+        case true:
+          return (
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          );
+        case false:
+          return <LoginForm />;
+        default:
+          return <Spinner size="large" />;
+      }
     }
-  }
+
 
   render() {
-    return (
+    return(
       <View>
         <Header headerText="Authentication" />
         {this.renderContent()}
